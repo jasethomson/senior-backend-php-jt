595394097386c6ef5cfb5c6f334b88fb3c9d8095
jasethomson <jasethomson31@gmail.com>
2019-08-20 11:03:36 -0700

    Completed php-includes-requires

---
+++ exercises/php_includes_requires/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function capWord( $word ){
+  $firstLetter = $word[0];
+  $capLetter = strtoupper($firstLetter);
+  $remainingLetters = substr( $word, 1);
+  $combinedWord = "{$capLetter}{$remainingLetters}";
+  return $combinedWord;
+}
+
+?>
