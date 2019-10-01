e939af03af73b43d63befa5b134f1257331e5845
jasethomson <jasethomson31@gmail.com>
2019-10-01 15:22:55 -0700

    Completed functions.php assignment.

--- exercises/php_functions/index.php
+++ exercises/php_functions/index.php
@@ -16,6 +16,26 @@ function doMath($num1, $num2, $op){
 
 $answer1 = doMath(2,4,'+');
 $answer2 = doMath('2','4', '*');
-print( $answer1 . "<br>" . $answer2);
+print( "answer1: " . $answer1 . "<br>" . "answer2: " . $answer2);
+
+$people = ['Jake', 'Jane', 'Dirk', 'Dane'];
+
+
+function findPerson($name){
+  global $people;
+  $peopleLength = count($people);
+  $currentPosition = 0;
+  while($currentPosition < $peopleLength && $people[$currentPosition] !== $name){
+    $currentPosition++;
+  }
+  if($currentPosition < $peopleLength){
+    return $currentPosition;
+  }
+  return -1;
+}
+
+$dirkPosition = findPerson('Dirk');
+$karlPosition = findPerson('Karl');
+print("<br>" . "Dirk was found at position " . $dirkPosition . " and Karl was found at position " . $karlPosition);
 
 ?>
