6a4829dabcb1abea6a72179632d5c9d99d37dbf0
jasethomson <jasethomson31@gmail.com>
2019-10-01 13:50:57 -0700

    Completed part 1 of if/switch assignment, got current utc and stated whether seconds were odd or even.

--- exercises/php_if_switch/index.php
+++ exercises/php_if_switch/index.php
@@ -1,32 +1,10 @@
 <?php
 
-
-$currentTime = microtime(true);
-$integerValue = intval($currentTime);
-
-if ($integerValue % 2 === 1){
-  print("{$integerValue} is odd");
+$currentTime = intval(microtime(true));
+if($currentTime % 2 === 1){
+  print($currentTime . " is odd");
 } else {
-  print("{$integerValue} is even");
+  print($currentTime . " is even");
 }
-
-$dateString = date("n-j");
-$message = "";
-
-switch($dateString){
-  case "12-25":
-    $message = "merry christmas";
-    break;
-  case "1-1":
-    $message = "happy new year";
-    break;
-  case "2-14":
-    $message = "happy valentine\'s day";
-    break;
-  case "7-4":
-    $message = "fireworks\! yay\!";
-    break;
-}
-print( $message );
 
 ?>
