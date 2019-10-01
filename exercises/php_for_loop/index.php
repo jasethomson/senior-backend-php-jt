7a8d82e79d82d532186015d0085bd31aab6d3afb
jasethomson <jasethomson31@gmail.com>
2019-10-01 14:54:15 -0700

    Completed part two and the entire for-loops assignment.

--- exercises/php_for_loop/index.php
+++ exercises/php_for_loop/index.php
@@ -5,4 +5,24 @@ $arrayLength = count($people);
 for($peopleI = 0; $peopleI < $arrayLength; $peopleI++){
   print("<div>" . $people[$peopleI] . "</div>");
 }
+
+$film = [
+  'title'=>'The Shawshank Redemption',
+  'year'=> 1994,
+  'director'=> 'Frank Darabont',
+  'cast'=> [
+    '        Andy Dufresnne'=> 'Tim Robbins',
+    '        Ellis "Red" Redding'=> 'Morgan Freeman',
+    '        Warden Norton'=> 'Bob Gunton'
+  ]
+];
+
+$sentence = $film['title'] . " was made in " . $film['year'] . " and directed by " . $film['director'] . ".<br><br>" . "Cast: ";
+$output = '';
+foreach( $film['cast'] as $key => $value ){
+  $output = $output . "<br>" . $key . " - " . $value;
+}
+
+print("<pre>" . $sentence . $output . "</pre>");
+
 ?>
